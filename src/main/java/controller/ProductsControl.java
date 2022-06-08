bc7abbe2fff87ca7496c15fc90387826a26d288d
shokirov <shokirovzokirjon>
2022-06-08 18:31:18 +0500

    rd

--- src/main/java/controller/ProductsControl.java
+++ src/main/java/controller/ProductsControl.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import static hucode.config.WebMvcConfig.jdbcTemplate;
+
 
 @Controller
 @RequiredArgsConstructor
